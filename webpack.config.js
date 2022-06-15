d9b165b79f52a3a5ec9dd0e26df5a2bbfd6fbc0e
ajiken4610 <86188770+ajiken4610@users.noreply.github.com>
2022-06-15 10:40:50 +0900

    glbのモデル読み込み

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,9 @@ const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
 
-const stylesHandler = isProduction
-  ? MiniCssExtractPlugin.loader
-  : "style-loader";
+const stylesHandler = isProduction ?
+  MiniCssExtractPlugin.loader :
+  "style-loader";
 
 const config = {
   entry: "./src/index.ts",
@@ -29,8 +29,7 @@ const config = {
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.(ts|tsx)$/i,
         loader: "ts-loader",
         exclude: ["/node_modules/"],
@@ -44,8 +43,11 @@ const config = {
         use: [stylesHandler, "css-loader", "postcss-loader"],
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|glb)$/i,
         type: "asset",
+        generator: {
+          filename: "assets/[name][ext]"
+        }
       },
 
       // Add your rules for custom modules here
